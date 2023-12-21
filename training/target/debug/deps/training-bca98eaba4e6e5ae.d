1d7ce5225a8e851f760c5634969b881f3c5a6cfc
JebaShinba <153792090+JebaShinba@users.noreply.github.com>
2023-12-21 13:45:19 +0530

    Initial COmit

---
+++ training/target/debug/deps/training-bca98eaba4e6e5ae.d
@@ -0,0 +1,5 @@
+D:\Rust training\Secondproject\training\target\debug\deps\libtraining-bca98eaba4e6e5ae.rmeta: src\main.rs
+
+D:\Rust training\Secondproject\training\target\debug\deps\training-bca98eaba4e6e5ae.d: src\main.rs
+
+src\main.rs:
