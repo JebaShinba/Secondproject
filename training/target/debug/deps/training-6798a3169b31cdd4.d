28c8c58b11be81ea368469768a9e176986991eef
JebaShinba <153792090+JebaShinba@users.noreply.github.com>
2023-12-22 17:06:04 +0530

    message changing

--- training/target/debug/deps/training-6798a3169b31cdd4.d
+++ training/target/debug/deps/training-6798a3169b31cdd4.d
@@ -1,5 +1,5 @@
-C:\Users\jebas\Secondproject\Secondproject\training\target\debug\deps\libtraining-6798a3169b31cdd4.rmeta: src\main.rs
+C:\Users\jebas\Secondproject\Secondproject-1\training\target\debug\deps\libtraining-6798a3169b31cdd4.rmeta: src\main.rs
 
-C:\Users\jebas\Secondproject\Secondproject\training\target\debug\deps\training-6798a3169b31cdd4.d: src\main.rs
+C:\Users\jebas\Secondproject\Secondproject-1\training\target\debug\deps\training-6798a3169b31cdd4.d: src\main.rs
 
 src\main.rs:
--- training/target/debug/deps/training-bca98eaba4e6e5ae.d
+++ training/target/debug/deps/training-bca98eaba4e6e5ae.d
@@ -1,5 +1,5 @@
-C:\Users\jebas\Secondproject\Secondproject\training\target\debug\deps\libtraining-bca98eaba4e6e5ae.rmeta: src\main.rs
+C:\Users\jebas\Secondproject\Secondproject-1\training\target\debug\deps\libtraining-bca98eaba4e6e5ae.rmeta: src\main.rs
 
-C:\Users\jebas\Secondproject\Secondproject\training\target\debug\deps\training-bca98eaba4e6e5ae.d: src\main.rs
+C:\Users\jebas\Secondproject\Secondproject-1\training\target\debug\deps\training-bca98eaba4e6e5ae.d: src\main.rs
 
 src\main.rs:
